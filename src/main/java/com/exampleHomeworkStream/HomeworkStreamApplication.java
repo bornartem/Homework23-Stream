500b2778c145509cd50a91720f89729b6b75e0c3
bornartem <born.artem@gmail.com>
2023-10-25 14:28:02 +0300

    1 commit

---
+++ src/main/java/com/exampleHomeworkStream/HomeworkStreamApplication.java
@@ -0,0 +1,13 @@
+package com.exampleHomeworkStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkStreamApplication.class, args);
+	}
+
+}
