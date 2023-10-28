9a6b9676fe62d918a9e98e7293e3459df264f67c
bornartem <born.artem@gmail.com>
2023-10-28 17:09:12 +0300

    2 commit

---
+++ src/main/java/com/exampleHomeworkStream/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.exampleHomeworkStream.service;
+
+
+import java.util.Collection;
+import java.util.Map;
+
+public interface EmployeeService {
+
+    Employee addEmployee(String firstName, String lastName, int salary, int department);
+
+    Employee removeEmployee(String firstName, String lastName, int salary, int department);
+
+    Employee findEmployee(String firstName, String lastName, int salary, int department);
+
+    Collection<Employee> getAll();
+    Map<String, Employee> getMap();
+}
